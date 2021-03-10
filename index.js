fb754af04e6a0b5dd8bac337308403db5b492852
Yitzchok Kimelman <itchkimel@gmail.com>
2021-03-09 23:30:06 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const employee = {
+  name: 'President Biden', 
+  streetAddress: 'White House',
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return Object.assign({}, employee, {[key]: value});
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  return Object.assign(employee, {[key]: value});
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = Object.assign({}, employee);
+  delete newEmployee[key];
+  return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+};
+
